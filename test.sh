0f46911356ef07e2d23bc02a6e5d6f7a526476b6
kittyandrew <kitty@maudrew.dev>
2024-03-16 16:27:39 +0000

    improve test script

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-curl -sX POST -H "Content-Type: application/json" -d "{\"username\": \"$1\", \"reponame\": \"$2\", \"provider\": \"${3:-github}\"}" 0.0.0.0:8080/api/jobs
+
+curl -sX POST -H "Content-Type: application/json" \
+    -d "{\"username\": \"${1:-kittyandrew}\", \"reponame\": \"${2:-klocc}\", \"provider\": \"${3:-github}\"}" \
+    0.0.0.0:8080/api/jobs
+
